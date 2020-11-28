015fee1d36ff4f18429069dbb40cf8df4d5bc994
Modestas Skirius <mode@Modestass-Mini.lan>
2020-11-28 15:58:48 +0200

    change button style

--- src/components/Button/Button.style.js
+++ src/components/Button/Button.style.js
@@ -1,30 +1,33 @@
 import styled from "styled-components";
 
 export const Button = styled.button`
-  background: ${(props) => 
-    (props.color === "primary" 
-      ? props.theme.primary.background 
-      : props.theme.secondary.background )};
-  color: ${(props) => 
-    (props.color === "primary" 
-      ? props.theme.primary.color 
-      : props.theme.secondary.color 
-  )};
+  background: ${(props) =>
+    props.color === "primary"
+      ? "linear-gradient(90deg, rgba(253,29,29, 1) 0%, rgba(252,125,69, 1) 100%)"
+      : props.theme.secondary.background};
+  color: ${(props) =>
+    props.color === "primary"
+      ? props.theme.primary.color
+      : props.theme.secondary.color};
   outline: none;
   border: none;
   border-radius: 5px;
-  padding: 0.5em 2em;
+  letter-spacing: 1px;
+  margin-top: 20px;
+  padding: 0.7em 2em;
+  transition: all 0.8s;
   cursor: pointer;
   &:hover {
-    background: ${(props) => 
-      props.color === "primary" 
-        ? props.theme.primary.hover.background 
-        : props.theme.secondary.hover.background };
+    transition: all 2s;
+    background: ${(props) =>
+      props.color === "primary"
+        ? "linear-gradient(180deg, rgba(253,29,29, 1) 0%, rgba(252,125,69, 1) 100%)"
+        : props.theme.secondary.hover.background};
   }
   &:focus {
-      background: ${(props) => 
-        props.color === "primary" 
-          ? props.theme.primary.focus.background 
-          : props.theme.secondary.focus.background };
+    background: ${(props) =>
+      props.color === "primary"
+        ? props.theme.primary.focus.background
+        : props.theme.secondary.focus.background};
   }
 `;
