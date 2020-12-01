76ee7044e887f50e965527f9a69cc326e84ede5c
Modestas Skirius <mode@Modestass-Mini.lan>
2020-12-01 17:21:32 +0200

    add privateroute

--- src/Routes.js
+++ src/Routes.js
@@ -1,22 +1,33 @@
-import React, { Suspense, lazy } from "react";
+import React, { Suspense, lazy, useContext } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-
-import { Header, Loading } from "./components";
+import { Header, Loading, PrivateRoute } from "./components";
 import { Login } from "./pages";
+import { AuthContext } from "./contexts/AuthContext";
 const RegisterLazy = lazy(() => import(`./pages/Register/Register`));
 const ViewBooksLazy = lazy(() => import(`./pages/ViewBooks/ViewBooks`));
 const AddBooksLazy = lazy(() => import(`./pages/AddBooks/AddBooks`));
 
 function Routes() {
+  const auth = useContext(AuthContext);
   return (
     <Router>
-      <Header />
+      <Header logged={!!auth} />
       <Suspense fallback={<Loading />}>
         <Switch>
           <Route exact path="/" component={Login} />
           <Route exact path="/register" component={RegisterLazy} />
-          <Route exact path="/viewbooks" component={ViewBooksLazy} />
-          <Route exact path="/addbooks" component={AddBooksLazy} />
+          <PrivateRoute
+            exact
+            path="/viewBooks"
+            redirectPath="/"
+            component={ViewBooksLazy}
+          />
+          <PrivateRoute
+            exact
+            redirectPath="/"
+            path="/addBooks"
+            component={AddBooksLazy}
+          />
         </Switch>
       </Suspense>
     </Router>
